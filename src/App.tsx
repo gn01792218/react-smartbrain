cc9bf7492f4a52f3cdb0645b5c027e3823a2375d
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-05-03 10:54:43 +0800

    上環境

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App bg-black text-red-700">
+      AAAAa
+      <header className="App-header bg-orange-400">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
