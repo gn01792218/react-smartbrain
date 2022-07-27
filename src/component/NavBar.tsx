d66200910da9204c0b9d92970cefef20053f57fd
MSI\gn01792218 <gn01792218@hotmail.com>
2022-07-27 16:08:50 +0800

    修正nav的logout沒有cursor-pointBug

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -85,7 +85,7 @@ function NavBar() {
                                                     key={item.name}
                                                     className={classNames(
                                                         item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                        'px-3 py-2 rounded-md text-sm font-medium'
+                                                        'px-3 py-2 rounded-md text-sm font-medium cursor-pointer'
                                                     )}
                                                     aria-current={item.current ? 'page' : undefined}
                                                 >
